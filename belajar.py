fdce05461b44f9fa1e871d68f17e45c452d56fc7
varelvarel <79013831+varelvarel@users.noreply.github.com>
2021-02-19 20:11:31 +0700

    Add files via upload

---
+++ belajar.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+a = 100
+print "Nilai a = ", a
+
+data_integer = 1000
+
+print "DataAsli Adalah : ", data_integer
+print "Jenis Data : ", type(data_integer)
+
+data = input("Nama Lu Apa tong? : ")
+print "Halo Beban Team ", data
